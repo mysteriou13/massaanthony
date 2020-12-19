816343752000a9a694fd6fbfecb6113027427bb8
mysteriou <elio007@hotmail.fr>
2020-12-19 18:04:42 +0100

    ajout lien dans le nav bar dans base.html

--- src/Controller/controlleur.php
+++ src/Controller/controlleur.php
@@ -5,7 +5,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
  class controlleur extends AbstractController{
  
- public function index(){
+ public function index($id){
 
    return  $this->render('./base.html');
 
