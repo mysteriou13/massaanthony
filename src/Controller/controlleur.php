4a7e73c72ab7a629c98f77eddc552c56bb63c42b
mysteriou <elio007@hotmail.fr>
2020-12-21 12:15:55 +0100

    correction des lien vers les page dans la template base.html

--- src/Controller/controlleur.php
+++ src/Controller/controlleur.php
@@ -6,20 +6,52 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
  class controlleur extends AbstractController{
  
  public function index(){
+  $pieces = explode("index.php", $this->hote());
 
-   return  $this->render('./index.html');
+  $url = $pieces[0]."index.php";
 
-
+  return $this->template();
   }
 
  public function page($id){
 
-   return  $this->render('./'.$id.'.html');
 
+  $pieces = explode("index.php", $this->hote());
+
+ $url = $pieces[0]."index.php";
+
+   
+return  $this->template();
 
   }
 
+     public function hote(){
 
+if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+{
+    $url = "https";
 }
+else
+{
+    $url = "http"; 
+}  
+$url .= "://"; 
+$url .= $_SERVER['HTTP_HOST']; 
+$url .= $_SERVER['PHP_SELF']; 
 
+return  $url;
+ 
+   }
+
+   public function template(){
+
+  $pieces = explode("index.php", $this->hote());
+
+ $url = $pieces[0]."index.php";
+
+  return  $this->render('./base.html',["index"=> $url, "project" => $url."/project", "propos"=> $url."/propos", 
+ "contact" => $url."/contact" ]);
+
+  }
+}
 ?>
